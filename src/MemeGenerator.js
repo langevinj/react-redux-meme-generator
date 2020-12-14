cc9355f75f601539862a97c03d1cbdabbd3ca908
Jeremy Langevin <langevinj832@gmail.com>
2020-12-14 14:13:16 -0500

    further styling

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useDispatch } from "react-redux";
 import { v4 as uuid} from 'uuid'
 
+import './MemeGenerator.css'
+
 function MemeGenerator() {
     const dispatch = useDispatch();
     const [formData, setFormData] = useState({
@@ -26,15 +28,23 @@ function MemeGenerator() {
 
     return (
         <div>
-            <h1>Make a meme:</h1>
+            <h1>Make a Meme:</h1>
             <form onSubmit={add}>
-                <label htmlFor="topText">Top Text:</label>
-                <textarea onChange={handleChange} name="topText" value={formData.topText} id="topText" row="4" cols="50"></textarea>
-                <label htmlFor="bottomText">Bottom Text:</label>
-                <textarea onChange={handleChange} name="bottomText" value={formData.bottomText} id="bottonText" row="4" cols="50"></textarea>
-                <label htmlFor="imgUrl">Image Url:</label>
-                <input type="text" name="imgUrl" value={formData.imgUrl} id="imgUrl" onChange={handleChange}></input>
-                <button>Create a meme</button>
+                <div>
+                    <label htmlFor="topText">Top Text:</label>
+                    <textarea onChange={handleChange} name="topText" value={formData.topText} id="topText" row="4" cols="50"></textarea>   
+                </div>
+                <div>
+                    <label htmlFor="bottomText">Bottom Text:</label>
+                    <textarea onChange={handleChange} name="bottomText" value={formData.bottomText} id="bottonText" row="4" cols="50"></textarea>
+                </div>
+                <div>
+                    <label htmlFor="imgUrl">Image Url:</label>
+                    <input type="text" name="imgUrl" value={formData.imgUrl} id="imgUrl" onChange={handleChange}></input>
+                </div>
+                <div>
+                    <button>Create a meme</button>
+                </div>
             </form>
         </div>
     )
